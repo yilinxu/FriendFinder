13c64f7dce204752c4e27110306d75d841c6d043
yilinxu <yellen10@gmail.com>
2017-05-05 11:17:08 -0500

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
